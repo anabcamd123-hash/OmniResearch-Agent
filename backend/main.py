822e9055770e61a18302aec58c5492c1b9530ed1
ikun1412 <scp0917@outlook.com>
2026-05-20 17:44:59 +0800

    Add WebSocket router to FastAPI application

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
 from backend.api.routes_task import router as task_router
+from backend.api.routes_ws import router as ws_router
 
 app = FastAPI()
 
 app.include_router(task_router)
-
+app.include_router(ws_router)
 @app.get("/")
 async def root():
     return {
